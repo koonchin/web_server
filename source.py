dccae7320fe99f9d4fb1df4226ad6be6da1812bd
unknown <koonchino@gmail.com>
2021-11-01 13:21:19 +0700

    update

---
+++ source.py
@@ -0,0 +1,17 @@
+# access mysql
+import os
+from pathlib import Path
+
+hostdb = '139.162.37.6'
+
+passworddb = 'Chino002'
+
+databasedb = 'image'
+
+userdb = 'gink'
+
+host = '127.0.0.1:8000'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
